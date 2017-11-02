8e8bfee369fd2678e170c645a2a0da048d87922e
Alexander Ruiz <AlexanderRuiz@Alexanders-MacBook-Air.local>
2017-11-02 03:01:50 -0200

    ajuste na formula

--- js/index.js
+++ js/index.js
@@ -5,9 +5,11 @@ $('#calcular').click(function(){
   var val2 = parseFloat($("#val2").val());
   var val3 = parseFloat($("#val3").val());
   
-  
-  
-  var formula = Math.sqrt(((((val1+val2+val3)/100)*2)/9.8))*100;
+  var media = ((val1+val2+val3)/3)/100;
+
+
+  var formula = Math.sqrt((2*media)/9.8);
+  // var formula = Math.sqrt((((((val1+val2+val3)/3)/100)*2)/9.8))*1000;
   console.log(formula);
   $('strong span').text(formula);
 });
